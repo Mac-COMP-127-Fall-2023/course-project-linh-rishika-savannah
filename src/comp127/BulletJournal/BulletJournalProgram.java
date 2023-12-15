943561a7f939f480e6cc1d1cea1dca12c1df581a
RishikaKundu <144388152+RishikaKundu@users.noreply.github.com>
2023-12-15 12:43:03 -0600

    commit

--- src/comp127/BulletJournal/BulletJournalProgram.java
+++ src/comp127/BulletJournal/BulletJournalProgram.java
@@ -5,6 +5,7 @@ import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.GraphicsObject;
 import edu.macalester.graphics.Rectangle;
+import edu.macalester.graphics.ui.Button;
 
 import java.awt.Color;
 import java.util.List;
@@ -19,79 +20,38 @@ public class BulletJournalProgram {
 
     public BulletJournalProgram(double size){
 
-        canvas= new CanvasWindow("Bullet Journal",
-            (int) Math.round(largeWidgetSize + miniWidgetSize),
-            (int) Math.round(largeWidgetSize));
+        canvas= new CanvasWindow("Bullet Journal",900,800);
+        sleepWidget=new SleepWidget(canvas);
         
-        this.largeWidgetSize = largeWidgetSize;
-        largeWidgets = createWidgets(largeWidgetSize);
-
-        miniWidgetSize = largeWidgetSize / largeWidgets.size();  // so they stack along one edge
-        miniWidgets = createWidgets(miniWidgetSize);
-
-
-
 
         canvas.setBackground(new Color(255, 189, 223));
-        waterWidget= new WaterWidget(600);
-        canvas.add(waterWidget.getGraphics());
+        addWidgetButtons();
+    }
 
-        taskWidget = new TaskWidget(size);
-        canvas.add(taskWidget.getGraphics());
+    private void addWidgetButtons(){
+        Button sleepbutton= new Button("Sleep Widget");
+        sleepbutton.setPosition(80, 110);
+        canvas.add(sleepbutton);
+        sleepbutton.onClick(()-> canvas.add(sleepWidget.getGraphics()));
 
-        sleepWidget = new SleepWidget(canvas);
-        canvas.add(sleepWidget.getGraphics());
+        Button waterbutton= new Button("Sleep Widget");
+        waterbutton.setPosition(80, 140);
+        canvas.add(sleepbutton);
+        sleepbutton.onClick(()-> canvas.add(waterWidget.getGraphics()));
 
-        selectionHighlight = new Rectangle(0, 0, miniWidgetSize, miniWidgetSize);  // selectWidgetAtIndex() will position it
-        selectionHighlight.setStroked(false);
-        selectionHighlight.setFillColor(new Color(0x7FFFFFFF, true));
-        canvas.add(selectionHighlight);
-
-
-        double y = 0;
-        for (BulletJournalWidget widget : miniWidgets) {
-            canvas.add(widget.getGraphics(), largeWidgetSize, y);
-            y += miniWidgetSize;
-        }
-
-        selectWidgetAtIndex(0);
         
 
-        canvas.onClick(event -> {
-            if (event.getPosition().getX() >= largeWidgetSize) {
-                selectWidgetAtIndex(
-                    (int) (event.getPosition().getY() / largeWidgetSize * miniWidgets.size()));
-            }
-        });
 
-        canvas.draw();
+
+
+        
         
     }
+      
+        
+        
 
-    private List<BulletJournalWidget> createWidgets(double size) {
-        return List.of(
-            new SleepWidget(canvas),
-            new WaterWidget(size),  
-            new MoodWidget(size));
-
-    }
-
-    private void selectWidgetAtIndex(int index) {
-
-        if ( index <0 || index> largeWidgets.size()){
-            return;
-        }
-
-        if(displayedLargeWidget!= null){
-            canvas.remove(displayedLargeWidget.getGraphics());
-        }
-        displayedLargeWidget= largeWidgets.get(index);
-       
-        if (displayedLargeWidget!= null){
-            canvas.add(displayedLargeWidget.getGraphics());
-        }
-        selectionHighlight.setPosition(largeWidgetSize, miniWidgetSize * index);
-    }
+   
     public static void main(String[] args) {
         BulletJournalProgram bjp = new BulletJournalProgram(600);
     }
